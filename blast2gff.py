6437fe24f2ac0f9cbc4b9051cb2cc2338fced69d
Rodrigo Milan Calegari <milan.calegari@unesp.br>
2020-08-06 21:05:31 -0300

    add blast2gff

---
+++ blast2gff.py
@@ -0,0 +1,22 @@
+import sys
+
+try:
+	inp = sys.argv[1]
+except:
+	inp = input('insert blast outp: ')
+
+data = open(inp, 'r').readlines()
+
+try:
+	name = sys.argv[2]
+except:
+	name = input('insert outp name: ')
+
+outp = opem(name,'w')
+
+for i in data:
+	s = i.split()
+	if s[8] < s[9]:
+		outp.write('%s	manual	%s	%s	%s	.	+	.	' %(s[1],s[0],s[8],s[9]))
+	else:
+		outp.write('%s	manual	%s	%s	%s	.	-	.	' %(s[1],s[0],s[9],s[8]))
